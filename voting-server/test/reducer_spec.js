6e5850180b625260859d7988da88f77e4e86234e
Michael Sverdlikovsky <michael.sverdlikovsky@ab-soft.net>
2016-02-12 16:44:58 +0200

    Server: test flow

--- voting-server/test/reducer_spec.js
+++ voting-server/test/reducer_spec.js
@@ -58,4 +58,20 @@ describe('reducer', () => {
             entries: []
         }));
     });
+
+    it('can be used with reduce', () => {
+        const actions = [
+            {type: 'SET_ENTRIES', entries: ['Trainspotting', '28 Days Later']},
+            {type: 'NEXT'},
+            {type: 'VOTE', entry: 'Trainspotting'},
+            {type: 'VOTE', entry: '28 Days Later'},
+            {type: 'VOTE', entry: 'Trainspotting'},
+            {type: 'NEXT'}
+        ];
+        const finalStep = actions.reduce(reducer, Map());
+
+        expect(finalStep).to.equal(fromJS({
+            winner: 'Trainspotting'
+        }));
+    })
 })
