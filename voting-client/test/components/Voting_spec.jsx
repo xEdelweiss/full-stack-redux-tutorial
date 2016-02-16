4d675ca498a607a2fada7d574caf7e95f3aad680
Michael Sverdlikovsky <xedelweiss@gmail.com>
2016-02-16 17:18:49 +0200

    Client: update tests

--- voting-client/test/components/Voting_spec.jsx
+++ voting-client/test/components/Voting_spec.jsx
@@ -1,13 +1,15 @@
-import React from 'react/addons';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
 import Voting from '../../src/components/Voting.jsx';
 import {expect} from 'chai';
 
-const {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate} = React.addons.TestUtils;
+const {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate} = TestUtils;
 
 describe('Voting', () => {
     it('renders a pair of buttons', () => {
         const component = renderIntoDocument(
-            <Voting pair={["Trainspotting", "28 Days Later"]} />
+            <Voting pair={["Trainspotting", "28 Days Later"]}/>
         );
         const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
 
@@ -22,11 +24,47 @@ describe('Voting', () => {
 
         const component = renderIntoDocument(
             <Voting pair={["Trainspotting", "28 Days Later"]}
-                    vote={vote} />
+                    vote={vote}/>
         );
         const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
         Simulate.click(buttons[0]);
 
         expect(votedWith).to.equal('Trainspotting');
+    });
+
+    it('disables buttons when user has voted', () => {
+        const component = renderIntoDocument(
+            <Voting pair={["Trainspotting", "28 Days Later"]}
+                    hasVoted="Trainspotting"/>
+        );
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+        expect(buttons.length).to.equal(2);
+        expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+        expect(buttons[1].hasAttribute('disabled')).to.equal(true);
+    });
+
+    it('adds label to the voted entry', () => {
+        const component = renderIntoDocument(
+            <Voting pair={["Trainspotting", "28 Days Later"]}
+                    hasVoted="Trainspotting"/>
+        );
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+        expect(buttons[0].textContent).to.contain('Voted');
+    });
+
+    it('renders just the winner when there is one', () => {
+        const component = renderIntoDocument(
+            <Voting pair={["Trainspotting", "28 Days Later"]}
+                    winner="Trainspotting"/>
+        );
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+        expect(buttons.length).to.equal(0);
+
+        const winner = ReactDOM.findDOMNode(component.refs.winner);
+        expect(winner).to.be.ok;
+        expect(winner.textContent).to.contain('Trainspotting');
     })
 });
