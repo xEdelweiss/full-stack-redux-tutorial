780a542796a36b156b18128801d78ed6e2112362
Michael Sverdlikovsky <xedelweiss@gmail.com>
2016-02-18 23:10:44 +0200

    Client: callback for next step

--- voting-client/test/components/Results_spec.jsx
+++ voting-client/test/components/Results_spec.jsx
@@ -5,7 +5,7 @@ import {List, Map} from 'immutable';
 import Results from '../../src/components/Results';
 import {expect} from 'chai';
 
-const {renderIntoDocument, scryRenderedDOMComponentsWithClass} = TestUtils;
+const {renderIntoDocument, scryRenderedDOMComponentsWithClass, Simulate} = TestUtils;
 
 describe('Results', () => {
     it('renders entries with vote counts or zero', () => {
@@ -22,5 +22,20 @@ describe('Results', () => {
         expect(train).to.contain('5');
         expect(days).to.contain('28 Days Later');
         expect(days).to.contain('0');
-    })
-})
+    });
+
+    it('invokes the next callback when next button is clicked', () => {
+        let nextInvoked = false;
+        const next = () => nextInvoked = true;
+
+        const pair = List.of('Trainspotting', '28 Days Later');
+        const component = renderIntoDocument(
+            <Results pair={pair}
+                     tally={Map()}
+                     next={next}/>
+        );
+        Simulate.click(ReactDOM.findDOMNode(component.refs.next));
+
+        expect(nextInvoked).to.equal(true);
+    });
+});
