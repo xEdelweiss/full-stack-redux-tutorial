67108ad7c8ee1f92bc58ee82232968becd0c6011
Michael Sverdlikovsky <xedelweiss@gmail.com>
2016-02-18 23:40:22 +0200

    Client: extracted tally to separated component

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -1,33 +1,16 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import Winner from './Winner.jsx';
+import Winner from './Winner';
+import Tally from './Tally'
 
 export default React.createClass({
     mixins: [PureRenderMixin],
-    getPair: function () {
-        return this.props.pair || [];
-    },
-    getVotes: function (entry) {
-        if (this.props.tally && this.props.tally.has(entry)) {
-            return this.props.tally.get(entry);
-        }
-
-        return 0;
-    },
     render: function () {
         return this.props.winner ?
             <Winner ref="winner" winner={this.props.winner}/> :
             <div className="results">
-                <div className="tally">
-                    {this.getPair().map(entry =>
-                        <div className="entry" key={entry}>
-                            <h1>{entry}</h1>
-                            <div className="voteCount">
-                                {this.getVotes(entry)}
-                            </div>
-                        </div>
-                    )}
-                </div>
+                <Tally pair={this.props.pair}
+                       tally={this.props.tally}/>
                 <div className="management">
                     <button className="next"
                             ref="next"
