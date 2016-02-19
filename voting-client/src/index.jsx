92d705660072e86275e819a8a8d2a7bd4bee90d1
Michael Sverdlikovsky <michael.sverdlikovsky@ab-soft.net>
2016-02-19 14:29:06 +0200

    Client: connect Voting with Redux

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route} from 'react-router';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
 import App from './components/App';
-import Voting from './components/Voting';
+import {VotingContainer} from './components/Voting';
 import Results from './components/Results';
+const store = createStore(reducer);
+store.dispatch({
+    type: 'SET_STATE',
+    state: {
+        vote: {
+            pair: ['Sunshine', '28 Days Later'],
+            tally: {Sunshine: 2}
+        }
+    }
+});
 
 const routes = (
     <Route component={App}>
         <Route path="/results" component={Results}/>
-        <Route path="/" component={Voting}/>
+        <Route path="/" component={VotingContainer}/>
     </Route>
 );
 
 ReactDOM.render(
-    <Router>{routes}</Router>,
+    <Provider store={store}>
+        <Router>{routes}</Router>
+    </Provider>,
     document.getElementById('app')
 );
