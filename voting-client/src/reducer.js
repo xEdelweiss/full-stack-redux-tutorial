5de88c48ed8e17b215bb746a208c8c5c1d0d5325
Michael Sverdlikovsky <michael.sverdlikovsky@ab-soft.net>
2016-02-19 15:12:54 +0200

    Client: reset hasVoted and fix npm test script

--- voting-client/src/reducer.js
+++ voting-client/src/reducer.js
@@ -1,4 +1,4 @@
-import {Map} from 'immutable';
+import {Map, List} from 'immutable';
 
 function setState(state, newState) {
     return state.merge(newState);
@@ -14,10 +14,22 @@ function vote(state, entry) {
     return state.set('hasVoted', entry);
 }
 
+function resetVote(state) {
+    const hasVoted = state.get('hasVoted');
+    const currentPair = state.getIn(['vote', 'pair'], List());
+
+    // @fixme wrong logic on penultimate voting
+    if (!hasVoted || currentPair.includes(hasVoted)) {
+        return state;
+    }
+
+    return state.remove('hasVoted');
+}
+
 export default function (state = Map(), action) {
     switch (action.type) {
         case 'SET_STATE':
-            return setState(state, action.state);
+            return resetVote(setState(state, action.state));
         case 'VOTE':
             return vote(state, action.entry);
     }
