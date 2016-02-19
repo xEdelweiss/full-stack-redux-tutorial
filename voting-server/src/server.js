0acdcfc0f8900c3c747652004102552f338e92f7
Michael Sverdlikovsky <michael.sverdlikovsky@ab-soft.net>
2016-02-19 14:55:48 +0200

    Server: 404 fix

--- voting-server/src/server.js
+++ voting-server/src/server.js
@@ -1,7 +1,7 @@
 import Server from 'socket.io';
 
 export default function startServer(store) {
-    const io = new Server().listen(8090, '0.0.0.0');
+    const io = new Server().attach(8090);
 
     store.subscribe(() => {
         io.emit('state', store.getState().toJS());
