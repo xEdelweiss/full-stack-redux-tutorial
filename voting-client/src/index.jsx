6948aa21c89d03958bf75367bda6c0cde434a817
Michael Sverdlikovsky <michael.sverdlikovsky@ab-soft.net>
2016-02-19 14:31:59 +0200

    Client: connect Results with Redux

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -6,8 +6,10 @@ import {Provider} from 'react-redux';
 import reducer from './reducer';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
-import Results from './components/Results';
+import {ResultsContainer} from './components/Results';
+
 const store = createStore(reducer);
+
 store.dispatch({
     type: 'SET_STATE',
     state: {
@@ -20,7 +22,7 @@ store.dispatch({
 
 const routes = (
     <Route component={App}>
-        <Route path="/results" component={Results}/>
+        <Route path="/results" component={ResultsContainer}/>
         <Route path="/" component={VotingContainer}/>
     </Route>
 );
